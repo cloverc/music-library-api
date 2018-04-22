17b274f500ff7d29ece3780503087c88c2c56d92
Clover Chapman <clo@cloverseeds.co.uk>
2018-04-22 11:44:56 +0100

    Defining an artist - step 2 completed

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
+const artist = require('./controllers/Artist');
 
+// mLab connection string
 require('dotenv').config({
   path: path.join(__dirname, './settings.env'),
 });
 
 const app = express();
+
+// connect to DB
 mongoose.connect(process.env.DATABASE_CONN);
+
 app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('Hello MongoDb!'));
+
+// POST route
+app.post('/Artist', artist.post);
 
 app.listen(3000, () => console.log('It works!'));
